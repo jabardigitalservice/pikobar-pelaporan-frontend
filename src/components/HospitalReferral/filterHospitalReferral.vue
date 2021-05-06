e34cd23e60cab92d8fd3c3010790455b5bf3f271
Arif9878 <arifboyz16@gmail.com>
2021-05-06 09:42:07 +0700

    date consistency

--- src/components/HospitalReferral/filterHospitalReferral.vue
+++ src/components/HospitalReferral/filterHospitalReferral.vue
@@ -160,7 +160,7 @@ export default {
   },
   data() {
     return {
-      formatDate: 'YYYY-MM-DD',
+      formatDate: 'YYYY/MM/DD',
       disabledDistrict: false,
       codeDistrict: '',
       nameVillage: '',
