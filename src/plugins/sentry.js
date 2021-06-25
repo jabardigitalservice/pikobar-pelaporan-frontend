2c814f788f6c99ffb65cb4a543a23e070f5d2c9a
Arif9878 <arifboyz16@gmail.com>
2021-06-25 10:06:09 +0700

    change browser tracing

--- src/plugins/sentry.js
+++ src/plugins/sentry.js
@@ -1,5 +1,5 @@
 import * as Sentry from '@sentry/vue'
-import { Integrations } from '@sentry/tracing'
+import { BrowserTracing } from '@sentry/tracing/esm/browser'
 
 export default {
   install(Vue) {
@@ -8,7 +8,7 @@ export default {
       dsn: process.env.VUE_APP_SENTRY_DSN,
       release: process.env.VUE_APP_VERSION,
       environment: process.env.VUE_APP_ERROR_ENVIRONMENT,
-      integrations: [new Integrations.BrowserTracing()],
+      integrations: [new BrowserTracing()],
       tracesSampleRate: parseFloat(process.env.VUE_APP_TRACES_SAMPLE_RATE),
       tracingOptions: {
         trackComponents: true
